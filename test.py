667574a13ac3f4f9793f98d0777c1c7ecc89da0d
David Jaimes <david.jaimes@jukinmedia.com>
2017-08-29 11:36:20 -0700

    some testing

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print("Testing python versioning")
+name = "dAvid jaImes"
+print(name.title())
+print(name.upper())
+print(name.lower())
+string_1 = "Hello"
+string_2 = "there"
+
+print(string_1 + " " + string_2)
+message = string_1 + " " + string_2 + " " + name.title()
+print(message)
